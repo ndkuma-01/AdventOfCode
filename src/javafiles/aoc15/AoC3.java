6df23083e4e53af8b1e362b36e198ba3a07ef854
Kumar <nikhil.kumar018@topper.wku.edu>
2023-02-09 12:02:16 -0600

    added AoC3 for 2015

---
+++ src/javafiles/aoc15/AoC3.java
@@ -0,0 +1,85 @@
+package javafiles.aoc15;
+
+import Utilities.General.DAYID;
+import Utilities.General.GetInputs;
+import Utilities.General.RUNDAY;
+import Utilities.aoc15.HyperDuplex;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class AoC3 extends RUNDAY implements DAYID {
+    @Override
+    public String p1() throws FileNotFoundException {
+        GetInputs input = new GetInputs(15,3);
+        int cx = 0;
+        int cy = 0;
+
+        HashSet<HyperDuplex> houses = new HashSet<HyperDuplex>();
+
+        String inp = input.wholeFile();
+        houses.add(new HyperDuplex(cx,cy));
+        for(int i = 0 ; i < inp.length(); i++){
+            if(inp.substring(i,i+1).equals(">")){
+                cx++;
+            }else if(inp.substring(i,i+1).equals("<")){
+                cx--;
+            }else if(inp.substring(i,i+1).equals("^")){
+                cy++;
+            }else if(inp.substring(i,i+1).equals("v")){
+                cy--;
+            }
+
+            houses.add(new HyperDuplex(cx,cy));
+        }
+        return String.valueOf(houses.size());
+    }
+
+    @Override
+
+    public String p2() throws FileNotFoundException {
+        GetInputs input = new GetInputs(15,3);
+        int cx = 0;
+        int cy = 0;
+        int rx = 0;
+        int ry = 0;
+
+        HashSet<HyperDuplex> houses = new HashSet<HyperDuplex>();
+
+        String inp = input.wholeFile();
+        houses.add(new HyperDuplex(cx,cy));
+        for(int i = 0 ; i < inp.length(); i++){
+            if(i%2 !=0) {
+                if (inp.substring(i, i + 1).equals(">")) {
+                    cx++;
+                } else if (inp.substring(i, i + 1).equals("<")) {
+                    cx--;
+                } else if (inp.substring(i, i + 1).equals("^")) {
+                    cy++;
+                } else if (inp.substring(i, i + 1).equals("v")) {
+                    cy--;
+                }
+                houses.add(new HyperDuplex(cx,cy));
+
+            }else{
+                if (inp.substring(i, i + 1).equals(">")) {
+                    rx++;
+                } else if (inp.substring(i, i + 1).equals("<")) {
+                    rx--;
+                } else if (inp.substring(i, i + 1).equals("^")) {
+                    ry++;
+                } else if (inp.substring(i, i + 1).equals("v")) {
+                    ry--;
+                }
+                houses.add(new HyperDuplex(rx, ry));
+            }
+        }
+
+        return String.valueOf(houses.size());
+    }
+
+    public static void main(String[] args) throws FileNotFoundException{
+        RUNDAY.run(new AoC3());
+    }
+}
