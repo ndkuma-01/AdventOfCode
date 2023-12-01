964c575a7a361ee074455ab23095b72b54a883c5
ndkuma-01 <nikhildkumar7@gmail.com>
2023-12-01 10:58:02 -0600

    Made P1 more functionally programmed added some functional elements to P2 as well

--- src/javafiles/aoc23/AoC1.java
+++ src/javafiles/aoc23/AoC1.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AoC1 implements DAYID {
@@ -16,12 +17,9 @@ public class AoC1 implements DAYID {
         GetInputs input = new GetInputs(23, 1);
         ArrayList<Integer> nums = new ArrayList<Integer>();
         while (input.hasLines()) {
-            String line = input.nextLine();
-            String result = "";
-            List<Character> temp = IntStream.range(0, line.length()).mapToObj(i -> line.charAt(i)).filter(i -> i - '0' >= 0 && i - '0' <= 9).toList();
-            for(int i = 0 ; i < temp.size(); i++) {
-                result += "" + temp.get(i);
-            }
+            String line = input.nextLine(), result = "";
+            List<Character> temp = IntStream.range(0, line.length()).mapToObj(line::charAt).filter(i -> i - '0' >= 0 && i - '0' <= 9).toList();
+            result += "" + temp.stream().map(String::valueOf).collect(Collectors.joining(""));
             nums.add(Integer.parseInt(result.substring(0, 1) + result.substring(result.length() - 1)));
         }
         return String.valueOf(nums.stream().mapToInt(x -> x).sum());
@@ -32,8 +30,7 @@ public class AoC1 implements DAYID {
         ArrayList<Integer> nums = new ArrayList<Integer>();
         String[] arr = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         while (input.hasLines()) {
-            String line = input.nextLine();
-            String result = "";
+            String line = input.nextLine(), result = "";
             for (int i = 0; i < line.length(); i++) {
                  result += (line.charAt(i) - '0' >= 0 && line.charAt(i) - '0' <= 9)? "" + line.charAt(i) : "";
                 String curr = line.substring(i);
